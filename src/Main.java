46e9f2b3abb21f6d055d237be4ff374677a644d4
unknown <Student@L403-18.carbon.cti.p.lodz.pl>
2019-04-18 14:52:17 +0200

    hadi97 dodanie

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("PowerRangers:\nMikołaj Baran: developer\nIgor Jarek: developer\nKarolina Płóciennik: tester\nAbdalhadi Alwazir: developer\nOlena Hrynevych: operations ");
         System.out.println("GitHub ID : igorJarek");
         System.out.println("Crivell\n");
+        System.out.println("hadi97\n");
     }
 }
