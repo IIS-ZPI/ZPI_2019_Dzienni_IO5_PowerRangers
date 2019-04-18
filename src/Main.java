b60894e7c32ea2690148074528ce89f7adbe4e6a
hadi <hadielwazer97@gmail.com>
2019-04-18 15:21:02 +0200

    implementing interface Multiplication

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-public class Main {
+public class Main implements IArithmeticsMult {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("PowerRangers:\nMikołaj Baran: developer\nIgor Jarek: developer\nKarolina Płóciennik: tester\nAbdalhadi Alwazir: developer\nOlena Hrynevych: operations ");
         System.out.println("GitHub ID : igorJarek");
         System.out.println("Crivell\n");
         System.out.println("hadi97\n");
         System.out.println("Lola9614\n");
+    }
+
+    @Override
+    public double Multiplication(double A, double B) {
+        return A*B;
     }
 }
