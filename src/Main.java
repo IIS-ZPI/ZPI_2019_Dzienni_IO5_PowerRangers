f6cb458474f9384b5f620c8fdd0fe229ba4ddf9f
Crivell <tarralan@wp.pl>
2019-04-18 14:23:50 +0200

    Dodanie Interface i jego implementacji

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-public class Main implements IArithmeticsMult {
+public class Main implements IArithmeticsDiff, IArithmeticsMult {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         System.out.println("PowerRangers:\nMikołaj Baran: developer\nIgor Jarek: developer\nKarolina Płóciennik: tester\nAbdalhadi Alwazir: developer\nOlena Hrynevych: operations ");
         System.out.println("GitHub ID : igorJarek");
         System.out.println("Crivell\n");
@@ -11,5 +11,10 @@ public class Main implements IArithmeticsMult {
     @Override
     public double Multiplication(double A, double B) {
         return A*B;
+    }
+
+    @Override
+    public double Difference(double A, double B) {
+        return A-B;
     }
 }
