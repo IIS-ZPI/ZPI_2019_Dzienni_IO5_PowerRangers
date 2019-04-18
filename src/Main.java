11bec866dad436821fe094ad6360d1cd21dc18b5
unknown <201953@edu.p.lodz.pl>
2019-04-18 13:42:33 +0200

    Add print GitHub ID

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("PowerRangers:\nMikołaj Baran: developer\nIgor Jarek: developer\nKarolina Płóciennik: tester\nAbdalhadi Alwazir: developer\nOlena Hrynevych: operations ");
+        System.out.println("GitHub ID : igorJarek");
         System.out.println("Crivell\n");
     }
 }
