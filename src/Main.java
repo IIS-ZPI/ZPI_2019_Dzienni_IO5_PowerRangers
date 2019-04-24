c0f7cdbbd94e2e1beb082f2b4c1a107197cf6339
hadi97 <hadielwazer97@gmail.com>
2019-04-24 21:34:25 +0200

    comment_one

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main implements IArithmeticDiv, IArithmeticsDiff, IArithmeticsMult,
         System.out.println("PowerRangers:\nMikołaj Baran: developer\nIgor Jarek: developer\nKarolina Płóciennik: tester\nAbdalhadi Alwazir: developer\nOlena Hrynevych: operations ");
         System.out.println("GitHub ID : igorJarek");
         System.out.println("Crivell\n");
-        System.out.println("hadi97\n");
+        System.out.println("hadi97\n"); //comentOne_Hadi
         System.out.println("Lola9614\n");
     }
 
