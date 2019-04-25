fb6eec7fc8b6d193f0d7898b43c03c751f2e7208
Igor <anicaots@gmail.com>
2019-04-25 11:19:32 +0200

    Revert "add comment"
    This reverts commit e16d8bb0824744c20b8c25010ec59e558f602230.

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main implements IArithmeticDiv, IArithmeticsDiff, IArithmeticsMult,
     }
 
     @Override
-    public double Addition(double A, double B) { /* dodawanie */
+    public double Addition(double A, double B) {
         return A+B;
     }
 
