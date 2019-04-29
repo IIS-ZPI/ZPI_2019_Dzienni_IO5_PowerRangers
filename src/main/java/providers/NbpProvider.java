fabc596d174ec7f71a64cdca8fc2fe497e274984
Mikołaj Baran <tarralan@wp.pl>
2019-04-29 20:29:28 +0200

    Pobranie tabeli A z servera nbp oraz rzutowanie ich z jsona na objekt javy

--- src/main/java/providers/NbpProvider.java
+++ src/main/java/providers/NbpProvider.java
@@ -1,4 +1,25 @@
 package providers;
 
+import com.google.gson.Gson;
+import org.springframework.web.client.RestTemplate;
+import providers.data.NbpTableA;
+
+import java.util.List;
+
 public class NbpProvider {
+
+    public static void main(String[] args) {
+        getNbpTableA();
+    }
+
+    private static void getNbpTableA()
+    {
+        final String uri = "http://api.nbp.pl/api/exchangerates/tables/A/";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        Gson gson = new Gson();
+        NbpTableA nbpTableA[] = gson.fromJson(result,NbpTableA[].class);
+        System.out.println(nbpTableA[0].getNo());
+    }
 }
