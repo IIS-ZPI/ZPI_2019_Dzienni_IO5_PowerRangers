3e8fc378da4659d495c8f6a993aac7b9d34aafda
Mikołaj Baran <tarralan@wp.pl>
2019-04-29 20:37:40 +0200

    Przekonwertowanie ze zwyklej tablicy NbpTableA na ArrayListe

--- src/main/java/providers/NbpProvider.java
+++ src/main/java/providers/NbpProvider.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import org.springframework.web.client.RestTemplate;
 import providers.data.NbpTableA;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NbpProvider {
@@ -12,14 +15,15 @@ public class NbpProvider {
         getNbpTableA();
     }
 
-    private static void getNbpTableA()
+    private static List<NbpTableA> getNbpTableA()
     {
         final String uri = "http://api.nbp.pl/api/exchangerates/tables/A/";
 
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
         Gson gson = new Gson();
-        NbpTableA nbpTableA[] = gson.fromJson(result,NbpTableA[].class);
-        System.out.println(nbpTableA[0].getNo());
+        ArrayList<NbpTableA> nbpTableA = new ArrayList<>(Arrays.asList(gson.fromJson(result,NbpTableA[].class)));
+        System.out.println(nbpTableA.get(0).getRates().get(0).getCode() + " " + nbpTableA.get(0).getRates().get(0).getMid());
+        return nbpTableA;
     }
 }
