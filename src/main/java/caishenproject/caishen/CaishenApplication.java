11fb9d6d2ae99763f1061bb89794d2add4aa390f
Crivell <tarralan@wp.pl>
2019-04-29 13:52:44 +0200

    Init Project

---
+++ src/main/java/caishenproject/caishen/CaishenApplication.java
@@ -0,0 +1,13 @@
+package caishenproject.caishen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaishenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaishenApplication.class, args);
+    }
+
+}
