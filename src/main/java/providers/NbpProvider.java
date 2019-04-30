514d5cfe0c35d402e4b82e3a7e278d0b1526b492
Mikołaj Baran <tarralan@wp.pl>
2019-04-30 18:11:20 +0200

    Dodanie endpointa ktor zwraca tabele A jako json /GET

--- src/main/java/providers/NbpProvider.java
+++ src/main/java/providers/NbpProvider.java
@@ -4,18 +4,14 @@ import com.google.gson.Gson;
 import org.springframework.web.client.RestTemplate;
 import providers.data.NbpTableA;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class NbpProvider {
 
-    public static void main(String[] args) {
-        getNbpTableA();
-    }
 
-    private static List<NbpTableA> getNbpTableA()
+    public List<NbpTableA> getNbpTableA()
     {
         final String uri = "http://api.nbp.pl/api/exchangerates/tables/A/";
 
@@ -23,7 +19,6 @@ public class NbpProvider {
         String result = restTemplate.getForObject(uri, String.class);
         Gson gson = new Gson();
         ArrayList<NbpTableA> nbpTableA = new ArrayList<>(Arrays.asList(gson.fromJson(result,NbpTableA[].class)));
-        System.out.println(nbpTableA.get(0).getRates().get(0).getCode() + " " + nbpTableA.get(0).getRates().get(0).getMid());
         return nbpTableA;
     }
 }
